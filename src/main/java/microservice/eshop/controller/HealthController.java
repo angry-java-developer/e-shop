5243787f058a1397acea457c3f69db11bb157da5
nik developer <->
2022-01-24 01:42:46 +0300

    first version e-shop microservice

---
+++ src/main/java/microservice/eshop/controller/HealthController.java
@@ -0,0 +1,19 @@
+package microservice.eshop.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    @GetMapping("health/")
+    public ResponseEntity<Map<String, String>> health() {
+        Map<String, String> map = new HashMap<>();
+        map.put("status", "ok");
+        return new ResponseEntity(map, HttpStatus.OK);
+    }
+}
