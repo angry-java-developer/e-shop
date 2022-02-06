225412ac94896df55691f9693c58fffa669178c0
nik developer <->
2022-02-07 02:50:05 +0300

    homework 3 changed health endpoint, configured springboot metrics

--- src/main/java/microservice/eshop/web/controller/HealthController.java
+++ src/main/java/microservice/eshop/web/controller/HealthController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class HealthController {
     @GetMapping("health/")
     public ResponseEntity<Map<String, String>> health() {
-        Map<String, String> map = new HashMap<>();
-        map.put("status", "ok");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        boolean isOk = new Random().nextBoolean();
+        if (isOk) {
+            Map<String, String> map = new HashMap<>();
+            map.put("status", "ok");
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
